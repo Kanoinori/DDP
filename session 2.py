978d78d14887cdca00cedaf5040a8ff6d4023149
Kanoinori <66674828+Kanoinori@users.noreply.github.com>
2024-01-10 10:13:57 +0000

    Add files via upload

---
+++ session 2.py
@@ -0,0 +1,46 @@
+def add(num1, num2):
+    return num1 + num2
+
+def substract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "ERROR: CANNOT DIVIDE BY ZERO"
+    else:
+        return num1 / num2
+    
+def calculator():
+    print("Select an operation:")
+    print("1. Add")
+    print("2. Substract")
+    print("3. Multiply")
+    print("4. Devide")
+
+    choice = input()
+
+    if choice == "1":
+        num1 = int(input("Enter number: "))
+        num2 = int(input("Enter number: "))
+        print(add(num1,num2))
+    
+    if choice == "2":
+        num1 = int(input("Enter number: "))
+        num2 = int(input("Enter number: "))
+        print(substract(num1, num2))
+    
+    if choice == "3":
+        num1 = int(input("Enter number: "))
+        num2 = int(input("Enter number: "))
+        print(multiply(num1, num2))
+
+    if choice == "4":
+        num1 = int(input("Enter number: "))
+        num2 = int(input("Enter number: "))
+        print(divide(num1, num2))
+
+        
+calculator()
