8e8d0a37a88a401155abe33938d558b78b70b31c
Kanoinori <66674828+Kanoinori@users.noreply.github.com>
2024-01-15 09:46:20 +0000

    Update and rename session 2.py to calculator.py
    rename the file and update it to the final version

--- calculator.py
+++ calculator.py
@@ -8,10 +8,13 @@ def multiply(num1, num2):
     return num1 * num2
 
 def divide(num1, num2):
-    if num2 == 0:
-        return "ERROR: CANNOT DIVIDE BY ZERO"
-    else:
+    if num2 != 0:
         return num1 / num2
+    else:
+        return "ERROR: CANNOT DIVIDE BY ZERO"
+
+def surplus(num1, num2):
+    return num1 % num2
     
 def calculator():
     print("Select an operation:")
@@ -19,6 +22,7 @@ def calculator():
     print("2. Substract")
     print("3. Multiply")
     print("4. Devide")
+    print("5. Surplus")
 
     choice = input()
 
@@ -42,5 +46,9 @@ def calculator():
         num2 = int(input("Enter number: "))
         print(divide(num1, num2))
 
-        
+    if choice == "5":
+        num1 = int(input("Enter number: "))
+        num2 = int(input("Enter number: "))
+        print(surplus(num1, num2))  
+
 calculator()
